869b5cb0473db1f42aa10ae80cec37a370a72f39
Hong-Tan <hongtan1422002@sgmail.com>
2022-03-27 10:45:32 +0700

    champion

--- src/Component/Interesting/interesting.tsx
+++ src/Component/Interesting/interesting.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './interesting.scss';
+import champion from './champion.json'
 
 function interesting() {
   return (
     <div id="other">
-      <h1 className="title-other">EPL, Có thể bạn chưa biết ?</h1>
+      <h1 className="title-other">Những nhà vô địch</h1>
+      <div className="list-champion">
+      {
+      
+          champion.map((value,key)=>{
+            const [displayDesc,setDisplayDesc]=useState(" ")
+            return(
+              <div className="img-overlay">
+              
+                <img src={value['url']} alt="" className="img-champ" onMouseOver={()=>setDisplayDesc(value['url'])} onMouseOut={()=>setDisplayDesc(' ')}/>
+                {displayDesc==value['url'] && <h1 className="des-champion">{value['des']}</h1>}
+                {displayDesc==' ' && <h1 className="des-champion-season">{value['season']}</h1>
+                }
+              </div>
+            );
+          })
+       
+      }
+       </div>
     </div>
   );
 }
